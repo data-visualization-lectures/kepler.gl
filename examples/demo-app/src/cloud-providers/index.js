5cbb3e540206794c1f9ebb0d7ac9c370c9c4005f
Yuichi Yazaki <visualizingjp@gmail.com>
2025-11-30 17:33:02 +0900

    Foursquareを保存対象から除く

--- examples/demo-app/src/cloud-providers/index.js
+++ examples/demo-app/src/cloud-providers/index.js
@@ -4,27 +4,14 @@
 import {CLOUD_PROVIDERS_CONFIGURATION} from '../constants/default-settings';
 
 import DropboxProvider from './dropbox/dropbox-provider';
-import FoursquareProvider from './foursquare/foursquare-provider';
 
-const {
-  DROPBOX_CLIENT_ID,
-  FOURSQUARE_CLIENT_ID,
-  FOURSQUARE_DOMAIN,
-  FOURSQUARE_API_URL,
-  FOURSQUARE_USER_MAPS_URL
-} = CLOUD_PROVIDERS_CONFIGURATION;
+const {DROPBOX_CLIENT_ID} = CLOUD_PROVIDERS_CONFIGURATION;
 
 const DROPBOX_CLIENT_NAME = 'Kepler.gl Demo App';
 
 export const DEFAULT_CLOUD_PROVIDER = 'dropbox';
 
 export const CLOUD_PROVIDERS = [
-  new FoursquareProvider({
-    clientId: FOURSQUARE_CLIENT_ID,
-    authDomain: FOURSQUARE_DOMAIN,
-    apiURL: FOURSQUARE_API_URL,
-    userMapsURL: FOURSQUARE_USER_MAPS_URL
-  }),
   new DropboxProvider(DROPBOX_CLIENT_ID, DROPBOX_CLIENT_NAME)
 ];
 
